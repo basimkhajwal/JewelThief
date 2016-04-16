569674177488fc1c7cf721253798087fb57d3f9e
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 23:02:22 +0100

    Added player camera movmene

--- js/world/world.js
+++ js/world/world.js
@@ -6,7 +6,7 @@ Game.World = Game.World || {};
 Game.World.World = function (width) {
     "use strict";
 
-    width = width || 1000;
+    width = width || 5000;
 
     var camera = Engine.Camera.create(0, 0),
         player = Game.World.Player(100, 200);
@@ -15,6 +15,10 @@ Game.World.World = function (width) {
 
         update: function (delta) {
             player.update(delta);
+
+            //Make camera follow player clamped to world bounds
+            var clamped = Math.max(500 + player.getWidth() / 2, Math.min(width - player.getWidth() / 2 - 500, player.getX()));
+            camera.setX(clamped - 500);
         },
 
         render: function (canvas) {
