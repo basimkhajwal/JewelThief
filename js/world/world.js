59fdfcd09ed584d72f301ba777728fb7fd8dd793
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 21:02:40 +0100

    Created base world

---
+++ js/world/world.js
@@ -0,0 +1,23 @@
+var Engine = Engine || {};
+var Game = Game || {};
+
+Game.World = Game.World || {};
+
+Game.World.World = function (width) {
+    "use strict";
+
+    width = width || 1000;
+
+    return {
+
+        update: function (delta) {
+
+        },
+
+        render: function (canvas) {
+
+        }
+
+    };
+
+};
