f6cd19c2f76ad2b75a0e9c34c8c3d14bd16b796a
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 20:28:47 +0100

    Added title test - it works :)

--- js/states/menu.js
+++ js/states/menu.js
@@ -6,7 +6,17 @@ Game.States = Game.States || {};
 Game.States.Menu = function () {
     "use strict";
 
-    var state = Engine.GameState.create();
+    var state = Engine.GameState.create(),
+
+        title = Engine.UI.TextArea.create(500, 100, "Jewel Thief", 20, "#000", "Monospace");
+
+    state.render = function (canvas) {
+        title.render(canvas);
+    };
+
+    state.update = function (delta) {
+
+    };
 
     return state;
 
