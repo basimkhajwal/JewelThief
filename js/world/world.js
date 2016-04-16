64bde98b6b8df6a239dce14d5a04a25c8572e839
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 21:06:15 +0100

    Added basic camera and test bed

--- js/world/world.js
+++ js/world/world.js
@@ -8,14 +8,22 @@ Game.World.World = function (width) {
 
     width = width || 1000;
 
+    var camera = Engine.Camera.create(0, 0);
+
     return {
 
         update: function (delta) {
-
+            camera.setX(camera.getX() + 20 * delta);
         },
 
         render: function (canvas) {
 
+            camera.projectContext(canvas);
+
+            canvas.fillStyle = "green";
+            canvas.fillRect(0, 0, 1000, 600);
+
+            camera.unProjectContext(canvas);
         }
 
     };
