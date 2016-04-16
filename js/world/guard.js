3566f0878eec83e376817e37c70e5093d1e2ad15
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 23:28:52 +0100

    Added base guard file and script loader

---
+++ js/world/guard.js
@@ -0,0 +1,36 @@
+var Engine = Engine || {};
+var Game = Game || {};
+
+Game.World = Game.World || {};
+
+Game.World.Guard = function (x, y) {
+    "use strict";
+
+    return {
+
+        update: function (delta) {
+
+        },
+
+        render: function (canvas) {
+
+        },
+
+        getX: function () {
+            return x;
+        },
+
+        getY: function () {
+            return y;
+        },
+
+        getWidth: function () {
+            return 40;
+        },
+
+        getHeight: function () {
+            return 40;
+        }
+    };
+
+};
