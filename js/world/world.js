82153b21ad0ba2eba31bed9b98701c844055e31b
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 21:18:15 +0100

    Added player to world

--- js/world/world.js
+++ js/world/world.js
@@ -8,12 +8,14 @@ Game.World.World = function (width) {
 
     width = width || 1000;
 
-    var camera = Engine.Camera.create(0, 0);
+    var camera = Engine.Camera.create(0, 0),
+        player = Game.World.Player(100, 200);
 
     return {
 
         update: function (delta) {
             camera.setX(camera.getX() + 20 * delta);
+            player.update(delta);
         },
 
         render: function (canvas) {
@@ -22,6 +24,8 @@ Game.World.World = function (width) {
 
             canvas.fillStyle = "green";
             canvas.fillRect(0, 0, 1000, 600);
+
+            player.render(canvas);
 
             camera.unProjectContext(canvas);
         }
