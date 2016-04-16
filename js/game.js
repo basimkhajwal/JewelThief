2ae003fccffcfd02f54a0388010f007e9bcb2449
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 20:17:10 +0100

    Added game file

---
+++ js/game.js
@@ -0,0 +1,20 @@
+var Engine = Engine || {};
+var Game = Game || {};
+
+//Call back to start a new game
+var startGame = function () {
+    "use strict";
+
+    var game = Engine.Game.create({
+        state: Game.States.Menu.create(),
+        width: 1000,
+        height: 600,
+        devmode: true
+    });
+
+    game.start();
+
+};
+
+//When the page loads, start the game
+window.addEventListener("load", startGame, false);
