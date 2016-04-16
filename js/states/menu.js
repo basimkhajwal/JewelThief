608259ae0de53879ef6dc12fc259eddef1652cd2
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 20:55:47 +0100

    Added game starting code

--- js/states/menu.js
+++ js/states/menu.js
@@ -7,15 +7,25 @@ Game.States.Menu = function () {
     "use strict";
 
     var state = Engine.GameState.create(),
+        game,
+        title = Engine.UI.TextArea.create(500, 100, "Jewel Thief (work in progress)", 20, "#000", "Monospace"),
+        startButton = Engine.UI.TextButton.create(400, 400, 200, 75, "Start Game");
 
-        title = Engine.UI.TextArea.create(500, 100, "Jewel Thief", 20, "#000", "Monospace");
+    startButton.setClickListener(function () {
+        game.getGameStateManager().setState();
+    });
+
+    state.onCreate = function (g) {
+        game = g;
+    };
 
     state.render = function (canvas) {
         title.render(canvas);
+        startButton.render(canvas);
     };
 
     state.update = function (delta) {
-
+        startButton.update();
     };
 
     return state;
