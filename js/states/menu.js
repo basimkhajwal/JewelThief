dfa3455d011376d57364c4a49bcb48b7cd3589ae
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 20:16:49 +0100

    Created simple menu (nothing!)

---
+++ js/states/menu.js
@@ -0,0 +1,13 @@
+var Game = Game || {};
+var Engine = Engine || {};
+
+Game.States = Game.States || {};
+
+Game.States.Menu = (function () {
+    "use strict";
+
+    var state = Engine.GameState.create();
+
+    return state;
+
+}());
