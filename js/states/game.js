740a9933e3460e5e9d68d1b915efb4d969e4e464
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-16 21:02:34 +0100

    Added world var

--- js/states/game.js
+++ js/states/game.js
@@ -6,7 +6,22 @@ Game.States = Game.States || {};
 Game.States.Game = function () {
     "use strict";
 
-    var state = Engine.GameState.create();
+    var state = Engine.GameState.create(),
+        game,
+
+        world = Game.World.World();
+
+    state.onCreate = function (g) {
+        game = g;
+    };
+
+    state.render = function (canvas) {
+        world.render(canvas);
+    };
+
+    state.update = function (delta) {
+        world.update(delta);
+    };
 
     return state;
 };
