59f3d47e8fe371e850a1a8288394c90cc8bf4b18
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-17 10:22:36 +0100

    Changed colours and added specks

--- js/world/world.js
+++ js/world/world.js
@@ -10,6 +10,15 @@ Game.World.World = function (width) {
 
     var camera = Engine.Camera.create(0, 0),
         player,
+        grassDots = (function () {
+            var g = [], i;
+
+            for (i = 0; i < 100; i += 1) {
+                g.push({x: 50 + Math.random() * (width - 100), y: Math.random() * 600 });
+            }
+
+            return g;
+        }()),
 
         world = {
 
@@ -23,10 +32,18 @@ Game.World.World = function (width) {
 
             render: function (canvas) {
 
-                canvas.fillStyle = "green";
+                var i;
+
+                canvas.fillStyle = "#e67e22";
                 canvas.fillRect(0, 0, 1000, 600);
 
                 camera.projectContext(canvas);
+
+                canvas.fillStyle = "#d35400";
+                for (i = 0; i < grassDots.length; i += 1) {
+                    canvas.fillRect(grassDots[i].x, grassDots[i].y, 5, 5);
+                }
+
                 player.render(canvas);
                 camera.unProjectContext(canvas);
             },
