b1a8f07f71e2ca23148af34016ac1094935af0b1
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-17 19:54:39 +0100

    Created gui for game over

--- js/states/gameOver.js
+++ js/states/gameOver.js
@@ -6,21 +6,66 @@ Game.States = Game.States || {};
 Game.States.GameOver = function (previousScreen) {
     "use strict";
 
-    var state = Engine.GameState.create();
+    var state = Engine.GameState.create(),
+        game,
+
+        title = Engine.UI.TextArea.create(500, 250, "Game Over", 65, "#2c3e50", "Lato", 900),
+
+        playAgain = Engine.UI.TextButton.create(300, 300, 400, 100, "Play Again"),
+        mainMenu = Engine.UI.TextButton.create(300, 450, 400, 100, "Main Menu"),
+
+        wasPlayPressed = false,
+        wasMainPressed = false;
+
+    playAgain.setCornerRadius(20);
+    mainMenu.setCornerRadius(20);
+
+    playAgain.setColour("#16a085");
+    playAgain.setClickColour("#16a085");
+    playAgain.setHoverColour("#1abc9c");
+    playAgain.getText().setFamily("Lato");
+    playAgain.getText().setSize(25);
+    playAgain.getText().setWeight(700);
+    playAgain.getText().setColour("#ecf0f1");
+
+    mainMenu.setColour("#16a085");
+    mainMenu.setClickColour("#16a085");
+    mainMenu.setHoverColour("#1abc9c");
+    mainMenu.getText().setFamily("Lato");
+    mainMenu.getText().setSize(25);
+    mainMenu.getText().setWeight(700);
+    mainMenu.getText().setColour("#ecf0f1");
+
+    state.onCreate = function (g) {
+        game = g;
+    };
 
     state.render = function (canvas) {
         canvas.putImageData(previousScreen, 0, 0);
 
-        canvas.globalAlpha = 0.2;
+        canvas.globalAlpha = 0.3;
         canvas.fillStyle = "#2c3e50";
         canvas.fillRect(0, 0, 1000, 600);
         canvas.globalAlpha = 1;
 
+        title.render(canvas);
+        playAgain.render(canvas);
+        mainMenu.render(canvas);
+
+        if (wasPlayPressed && !playAgain.isClicked()) {
+            game.getGameStateManager().setState(Game.States.Game());
+        }
+        wasPlayPressed = playAgain.isClicked();
+
+        if (wasMainPressed && !mainMenu.isClicked()) {
+            game.getGameStateManager().setState(Game.States.Menu());
+        }
+        wasMainPressed = mainMenu.isClicked();
     };
 
     state.update = function (delta) {
-
-
+        playAgain.update();
+        mainMenu.update();
     };
 
     return state;
