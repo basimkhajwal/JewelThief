5a2add1e80815e49cb56fead32d56aa5f35c6967
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-17 11:22:47 +0100

    Added bullet logic

--- js/world/player.js
+++ js/world/player.js
@@ -7,12 +7,15 @@ Game.World.Player = function (startX, startY, world) {
     "use strict";
 
     var clamp = function (x, a, b) {
-        return Math.max(a, Math.min(b, x));
-    };
+            return Math.max(a, Math.min(b, x));
+        },
+        wasSpaceDown = false,
+        wasPDown = false,
+        bulletDirection = 1;
 
     return {
 
-        entity: Game.World.Entity(startX, startY, 40, 40),
+        entity: Game.World.Entity(startX, startY, 40, 40, world),
 
         update: function (delta) {
 
@@ -25,10 +28,12 @@ Game.World.Player = function (startX, startY, world) {
 
             if (keyDown(Engine.Keys.RIGHT) || keyDown(getKey('D'))) {
                 vx += speed;
+                bulletDirection = 1;
             }
 
             if (keyDown(Engine.Keys.LEFT) || keyDown(getKey('A'))) {
                 vx -= speed;
+                bulletDirection = -1;
             }
 
             if (keyDown(Engine.Keys.UP) || keyDown(getKey('W'))) {
@@ -38,6 +43,23 @@ Game.World.Player = function (startX, startY, world) {
             if (keyDown(Engine.Keys.DOWN) || keyDown(getKey('S'))) {
                 vy += speed;
             }
+
+            if (!keyDown(Engine.Keys.SPACE) && wasSpaceDown) {
+                this.entity.shape = this.entity.shape === 'C' ? 'R' : 'C';
+                if (this.entity.shape === 'C') {
+                    this.entity.width = this.entity.height = 40;
+                    this.entity.colour = "#c0392b";
+                } else {
+                    this.entity.width = this.entity.height = 35;
+                    this.entity.colour = "#8e44ad";
+                }
+            }
+            wasSpaceDown = keyDown(Engine.Keys.SPACE);
+
+            if (!keyDown(getKey('P')) && wasPDown) {
+                this.entity.fireBullet(bulletDirection * 500, 3);
+            }
+            wasPDown = keyDown(getKey('P'));
 
             if (vx !== 0 && vy !== 0) {
                 vx /= Math.sqrt(2);
