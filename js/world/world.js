ff25457b6bfd6b4aaac303ca124d542648ffd46e
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-17 19:03:38 +0100

    Added beautiful end bit

--- js/world/world.js
+++ js/world/world.js
@@ -6,7 +6,7 @@ Game.World = Game.World || {};
 Game.World.World = function (width) {
     "use strict";
 
-    width = width || 5000;
+    width = width || 1000;
 
     var camera = Engine.Camera.create(0, 0),
         player,
@@ -21,6 +21,46 @@ Game.World.World = function (width) {
 
             return g;
         }()),
+
+        drawEnd = function (canvas) {
+
+            canvas.globalAlpha = 0.2;
+            canvas.fillStyle = "#2c3e50";
+            canvas.fillRect(width - 120, 0, 20, 600);
+            canvas.globalAlpha = 1;
+
+            canvas.fillStyle = "#95a5a6";
+            canvas.fillRect(width - 100, 0, 100, 600);
+
+            canvas.fillStyle = "#bdc3c7";
+            canvas.fillRect(width - 100, 550, 100, 50);
+
+            canvas.fillStyle = "#7f8c8d";
+            canvas.fillRect(width - 100, 550, 100, 10);
+
+            //x: 0-7, y : 0-20
+            var bricks = [
+                [1, 10],
+                [5, 15],
+                [3, 6],
+                [2, 20]
+            ];
+
+            canvas.strokeStyle = "#7f8c8d";
+            bricks.forEach(function (brick) {
+
+                canvas.globalAlpha = 0.2;
+                canvas.fillRect(width - 100 + brick[0] * 12.5, brick[1] * 25, 12.5, 25);
+                canvas.globalAlpha = 1;
+
+                canvas.beginPath();
+                canvas.moveTo(width - 100 + brick[0] * 12.5, brick[1] * 25);
+                canvas.lineTo(width - 100 + brick[0] * 12.5, brick[1] * 25 + 25);
+                canvas.lineTo(width - 100 + brick[0] * 12.5 + 12.5, brick[1] * 25 + 25);
+                canvas.stroke();
+                canvas.closePath();
+            });
+        },
 
         world = {
 
@@ -68,15 +108,22 @@ Game.World.World = function (width) {
                     canvas.fillRect(grassDots[i].x, grassDots[i].y, 5, 5);
                 }
 
+                canvas.fillStyle = "#34495e";
+                canvas.fillRect(width - 160, 250, 100, 100);
+                canvas.fillStyle = "#2c3e50";
+                canvas.fillRect(width - 135, 275, 100, 50);
+
                 for (i = 0; i < this.renderables.length; i += 1) {
                     this.renderables[i].render(canvas);
                 }
+
+                drawEnd(canvas);
 
                 camera.unProjectContext(canvas);
             },
 
             getWidth: function () {
-                return width;
+                return width - 100;
             },
 
             getPlayer: function () {
@@ -87,7 +134,7 @@ Game.World.World = function (width) {
     player =  Game.World.Player(100, 200, world);
     world.renderables.push(player);
 
-    for (i = 0; i < 15; i += 1) {
+    for (i = 0; i < 2; i += 1) {
         guard = Game.World.Guard(Math.random() * width, Math.random() * 600, world);
         world.renderables.push(guard);
         world.guards.push(guard);
