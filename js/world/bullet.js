0d266d1139f7388c8801a545c40ae4a7fcc56d61
Basim Khajwal <basimkhajwal@gmail.com>
2016-04-17 17:52:04 +0100

    Added changing colours

--- js/world/bullet.js
+++ js/world/bullet.js
@@ -39,7 +39,7 @@ Game.World.Bullet = function (x, y, angle, speed, lifeTime, isPlayer) {
             rotatedRect(canvas, x - 4, y + 2, 8, 4);
 
             canvas.globalAlpha = 1;
-            canvas.fillStyle = "#2980b9";
+            canvas.fillStyle = isPlayer ? "#2980b9" : "#27ae60";
             rotatedRect(canvas, x - 4, y - 2, 8, 4);
 
         },
